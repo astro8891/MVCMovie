e4483330b45500087506fb91f31f307d00186bf3
Ashley <Ashley>
2016-05-11 21:01:37 +1200

    init

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //default route is /[Controller]/[ActionName]/[Parameters]
+        //Controller methods are known as action methods - they return an ActionResult or something that derives from ActionResult
+
+        // 
+        // GET: /HelloWorld/ 
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        }
+    }
+}
